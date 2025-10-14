2b0a23dbb9971c9f1dd41ef97e0ebbcdb18cfc6d
Ricardo <rich.mtx1205@gmail.com>
2025-10-14 12:53:06 -0600

    Actualización

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Index } from './pages/index';
+import { Nosotros } from './pages/nosotros/nosotros';
+import { Contacto } from './pages/contacto/contacto';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Index},
+    {path: 'nosotros', component: Nosotros},
+    {path: 'contacto', component: Contacto}
+];
