15283436ec2c6976636b08dccebc8e8398601ffa
Lupita <guadalupe.hdz.contre@gmail.com>
2025-10-14 14:55:11 -0600

    actualuzacion

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  standalone: true,
   templateUrl: './contacto.html',
   styleUrl: './contacto.css'
 })
