86cdcf8bf28cd9308b8b1789a679af14fb7bd980
Ricardo <rich.mtx1205@gmail.com>
2025-10-14 14:27:28 -0600

    Actualizacion

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../../components/navbar/navbar";
 
 @Component({
   selector: 'app-index',
   standalone: true,
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
+  imports: [Navbar]
 })
 export class IndexComponent {
 
