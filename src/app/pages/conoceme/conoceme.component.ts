68e6b73d94f31ce309a0d4344df5b881194aafed
Ricardo <rich.mtx1205@gmail.com>
2025-10-15 11:33:44 -0600

    Commit2

--- src/app/pages/conoceme/conoceme.component.ts
+++ src/app/pages/conoceme/conoceme.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-conoceme',
-  imports: [Navbar],
+  imports: [Navbar, Footer],
   templateUrl: './conoceme.html',
   styleUrl: './conoceme.css'
 })
