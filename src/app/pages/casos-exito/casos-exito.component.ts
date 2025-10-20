cc188a7a882bc7a04cce4c15c32c51cbba5b7f2f
Lupita <guadalupe.hdz.contre@gmail.com>
2025-10-20 15:02:53 -0600

    casosexito

--- src/app/pages/casos-exito/casos-exito.component.ts
+++ src/app/pages/casos-exito/casos-exito.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { Navbar } from "../../components/navbar/navbar";
-import { Footer } from "../../components/footer/footer";
+import { Component, ElementRef, AfterViewInit } from '@angular/core';
+import { Navbar } from '../../components/navbar/navbar';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-casos-exito',
+  standalone: true,
   imports: [Navbar, Footer],
   templateUrl: './casos-exito.html',
-  styleUrl: './casos-exito.css'
+  styleUrls: ['./casos-exito.css']
 })
-export class CasosExito {
+export class CasosExito implements AfterViewInit {
+  constructor(private el: ElementRef) {}
 
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+          } else {
+            entry.target.classList.remove('show');
+          }
+        });
+      },
+      { threshold: 0.2 }
+    );
+
+    const elements = this.el.nativeElement.querySelectorAll('.fade-up');
+    elements.forEach((el: HTMLElement) => observer.observe(el));
+  }
 }
